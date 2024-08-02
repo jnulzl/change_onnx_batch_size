b615f95764f25fc9c52e181027300a21bf1d37ca
jnulzl <jnulzl@qq.com>
2024-08-02 16:48:17 +0800

    Update requirements.txt and .gitignore

---
+++ generate_exe.sh
@@ -0,0 +1,4 @@
+PYTHON_PACKAGES_PATH=/mnt/d/codes/Python/change_onnx_batch/changeOnnxBatch_env/lib/python3.9/site-packages
+# pyinstaller --add-data "${PYTHON_PACKAGES_PATH}/amct_onnx/custom_op/libamct_onnx_ops.so:amct_onnx/custom_op/" --add-data "${PYTHON_PACKAGES_PATH}/amct_onnx/lib/libamct_ncx.so:amct_onnx/lib/" --add-data "${PYTHON_PACKAGES_PATH}/amct_onnx/lib/libquant_onnx.so:amct_onnx/lib/" --add-data "${PYTHON_PACKAGES_PATH}/amct_onnx/.version:./amct_onnx" --add-data "${PYTHON_PACKAGES_PATH}/onnxruntime/capi/libonnxruntime_providers_shared.so:./onnxruntime/capi/" --add-data "${PYTHON_PACKAGES_PATH}/amct_onnx/capacity/capacity_config.csv:./amct_onnx/capacity/"  --onefile  /home/malong/jnulzl/Ascend310/amct_python3/bin/amct_onnx
+pyinstaller --add-data "${PYTHON_PACKAGES_PATH}/onnxruntime/capi/libonnxruntime_providers_shared.so:./onnxruntime/capi/" --onefile  $1
+# pyinstaller --onefile  $1
